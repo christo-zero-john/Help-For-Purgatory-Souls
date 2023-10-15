47214f86b24f6462124aaffa9baffe1beb14e07b
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2023-10-15 17:18:56 +0530

    commit 18 on 15 10 2023

---
+++ resources/js/prayerRoomData.js
@@ -0,0 +1,197 @@
+var data = {
+    novena:[
+        "Prayer of Day 1",
+        "Prayer of Day 2",
+        "Prayer of Day 3",
+        "Prayer of Day 4",
+        "Prayer of Day 5",
+        "Prayer of Day 6",
+        "Prayer of Day 7",
+        "Prayer of Day 8",
+        "Prayer of Day 9"
+    ],
+
+    vanakkamaasam:[
+        {
+            topic : "Topic of day 1",
+            article : "article of day 1",
+            prayer : "prayer of day 1",
+            sukrithajapam:"sukruthajapam of day 1"
+        },
+        {
+            topic : "Topic of day 2",
+            article : "article of day 2",
+            prayer : "prayer of day 2",
+            sukrithajapam:"sukruthajapam of day 2"
+        },
+        {
+            topic : "Topic of day 13",
+            article : "article of day 13",
+            prayer : "prayer of day 13",
+            sukrithajapam:"sukruthajapam of day 13"
+        },
+        {
+            topic : "Topic of day 14",
+            article : "article of day 14",
+            prayer : "prayer of day 14",
+            sukrithajapam:"sukruthajapam of day 14"
+        },
+        {
+            topic : "Topic of day 15",
+            article : "article of day 15",
+            prayer : "prayer of day 15",
+            sukrithajapam:"sukruthajapam of day 15"
+        },
+        {
+            topic : "Topic of day 16",
+            article : "article of day 16",
+            prayer : "prayer of day 16",
+            sukrithajapam:"sukruthajapam of day 16"
+        },
+        {
+            topic : "Topic of day 17",
+            article : "article of day 17",
+            prayer : "prayer of day 17",
+            sukrithajapam:"sukruthajapam of day 17"
+        },
+        {
+            topic : "Topic of day 18",
+            article : "article of day 18",
+            prayer : "prayer of day 18",
+            sukrithajapam:"sukruthajapam of day 18"
+        },
+        {
+            topic : "Topic of day 19",
+            article : "article of day 19",
+            prayer : "prayer of day 19",
+            sukrithajapam:"sukruthajapam of day 19"
+        },
+        {
+            topic : "Topic of day 20",
+            article : "article of day 20",
+            prayer : "prayer of day 20",
+            sukrithajapam:"sukruthajapam of day 20"
+        },
+        {
+            topic : "Topic of day 11",
+            article : "article of day 11",
+            prayer : "prayer of day 11",
+            sukrithajapam:"sukruthajapam of day 11"
+        },
+        {
+            topic : "Topic of day 12",
+            article : "article of day 12",
+            prayer : "prayer of day 12",
+            sukrithajapam:"sukruthajapam of day 12"
+        },
+        {
+            topic : "Topic of day 13",
+            article : "article of day 13",
+            prayer : "prayer of day 13",
+            sukrithajapam:"sukruthajapam of day 13"
+        },
+        {
+            topic : "Topic of day 14",
+            article : "article of day 14",
+            prayer : "prayer of day 14",
+            sukrithajapam:"sukruthajapam of day 14"
+        },
+        {
+            topic : "Topic of day 15",
+            article : "article of day 15",
+            prayer : "prayer of day 15",
+            sukrithajapam:"sukruthajapam of day 15"
+        },
+        {
+            topic : "Topic of day 16",
+            article : "article of day 16",
+            prayer : "prayer of day 16",
+            sukrithajapam:"sukruthajapam of day 16"
+        },
+        {
+            topic : "Topic of day 17",
+            article : "article of day 17",
+            prayer : "prayer of day 17",
+            sukrithajapam:"sukruthajapam of day 17"
+        },
+        {
+            topic : "Topic of day 18",
+            article : "article of day 18",
+            prayer : "prayer of day 18",
+            sukrithajapam:"sukruthajapam of day 18"
+        },
+        {
+            topic : "Topic of day 19",
+            article : "article of day 19",
+            prayer : "prayer of day 19",
+            sukrithajapam:"sukruthajapam of day 19"
+        },
+        {
+            topic : "Topic of day 20",
+            article : "article of day 20",
+            prayer : "prayer of day 20",
+            sukrithajapam:"sukruthajapam of day 20"
+        },
+        {
+            topic : "Topic of day 21",
+            article : "article of day 21",
+            prayer : "prayer of day 21",
+            sukrithajapam:"sukruthajapam of day 21"
+        },
+        {
+            topic : "Topic of day 22",
+            article : "article of day 22",
+            prayer : "prayer of day 22",
+            sukrithajapam:"sukruthajapam of day 22"
+        },
+        {
+            topic : "Topic of day 23",
+            article : "article of day 23",
+            prayer : "prayer of day 23",
+            sukrithajapam:"sukruthajapam of day 23"
+        },
+        {
+            topic : "Topic of day 24",
+            article : "article of day 24",
+            prayer : "prayer of day 24",
+            sukrithajapam:"sukruthajapam of day 24"
+        },
+        {
+            topic : "Topic of day 25",
+            article : "article of day 25",
+            prayer : "prayer of day 25",
+            sukrithajapam:"sukruthajapam of day 25"
+        },
+        {
+            topic : "Topic of day 26",
+            article : "article of day 26",
+            prayer : "prayer of day 26",
+            sukrithajapam:"sukruthajapam of day 26"
+        },
+        {
+            topic : "Topic of day 27",
+            article : "article of day 27",
+            prayer : "prayer of day 27",
+            sukrithajapam:"sukruthajapam of day 27"
+        },
+        {
+            topic : "Topic of day 28",
+            article : "article of day 28",
+            prayer : "prayer of day 28",
+            sukrithajapam:"sukruthajapam of day 28"
+        },
+        {
+            topic : "Topic of day 29",
+            article : "article of day 29",
+            prayer : "prayer of day 29",
+            sukrithajapam:"sukruthajapam of day 29"
+        },
+        {
+            topic : "Topic of day 30",
+            article : "article of day 30",
+            prayer : "prayer of day 30",
+            sukrithajapam:"sukruthajapam of day 30"
+        }
+    ]
+};
+
--- script.js
+++ script.js
@@ -1,35 +1,335 @@
-var headerText = `
-<header class="site-header sticky-top py-1 english">
-<nav class="navbar navbar-expand-lg navbar-light bg-light text-danger">
-  <a href="/index.html" class="d-md-none  text-secondary navHead">Help For Purgatory</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav mx-auto">
 
-        <li class="nav-item">
-            <a class="nav-link navLink" href="~/index.html">Home</a>
-        </li>
+var data = {
+  novena:[
+      "Prayer of Day 1",
+      "Prayer of Day 2",
+      "Prayer of Day 3",
+      "Prayer of Day 4",
+      "Prayer of Day 5",
+      "Prayer of Day 6",
+      "Prayer of Day 7",
+      "Prayer of Day 8",
+      "Prayer of Day 9"
+  ],
 
-      <li class="nav-item">
-        <a class="nav-link navLink" href="#">Prayer Room</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="~sitepages/main/aboutThem.html">Purgatory Souls</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="~sitepages/main/aboutThem.html/#howToAndWhyToPray">How to and Why to Pray</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#"></a>
-      </li>
-    </ul>
-  </div>
-</nav>
-</header>
+  vanakkamaasam:[
+      {
+          topic : "Topic of day 1",
+          advice : "advice of day 1",
+          prayer : "prayer of day 1",
+          sukrithajapam:"sukruthajapam of day 1",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 2",
+          advice : "advice of day 2",
+          prayer : "prayer of day 2",
+          sukrithajapam:"sukruthajapam of day 2",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 3",
+          advice : "advice of day 3",
+          prayer : "prayer of day 3",
+          sukrithajapam:"sukruthajapam of day 3",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 4",
+          advice : "advice of day 4",
+          prayer : "prayer of day 4",
+          sukrithajapam:"sukruthajapam of day 4",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 5",
+          advice : "advice of day 5",
+          prayer : "prayer of day 5",
+          sukrithajapam:"sukruthajapam of day 5",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 6",
+          advice : "advice of day 6",
+          prayer : "prayer of day 6",
+          sukrithajapam:"sukruthajapam of day 6",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 7",
+          advice : "advice of day 7",
+          prayer : "prayer of day 7",
+          sukrithajapam:"sukruthajapam of day 7",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 8",
+          advice : "advice of day 8",
+          prayer : "prayer of day 8",
+          sukrithajapam:"sukruthajapam of day 8",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 9",
+          advice : "advice of day 9",
+          prayer : "prayer of day 9",
+          sukrithajapam:"sukruthajapam of day 9",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 10",
+          advice : "advice of day 10",
+          prayer : "prayer of day 10",
+          sukrithajapam:"sukruthajapam of day 10",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 11",
+          advice : "advice of day 11",
+          prayer : "prayer of day 11",
+          sukrithajapam:"sukruthajapam of day 11",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 12",
+          advice : "advice of day 12",
+          prayer : "prayer of day 12",
+          sukrithajapam:"sukruthajapam of day 12",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 13",
+          advice : "advice of day 13",
+          prayer : "prayer of day 13",
+          sukrithajapam:"sukruthajapam of day 13",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 14",
+          advice : "advice of day 14",
+          prayer : "prayer of day 14",
+          sukrithajapam:"sukruthajapam of day 14",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 15",
+          advice : "advice of day 15",
+          prayer : "prayer of day 15",
+          sukrithajapam:"sukruthajapam of day 15",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 16",
+          advice : "advice of day 16",
+          prayer : "prayer of day 16",
+          sukrithajapam:"sukruthajapam of day 16",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 17",
+          advice : "advice of day 17",
+          prayer : "prayer of day 17",
+          sukrithajapam:"sukruthajapam of day 17",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 18",
+          advice : "advice of day 18",
+          prayer : "prayer of day 18",
+          sukrithajapam:"sukruthajapam of day 18",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 19",
+          advice : "advice of day 19",
+          prayer : "prayer of day 19",
+          sukrithajapam:"sukruthajapam of day 19",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 20",
+          advice : "advice of day 20",
+          prayer : "prayer of day 20",
+          sukrithajapam:"sukruthajapam of day 20",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 21",
+          advice : "advice of day 21",
+          prayer : "prayer of day 21",
+          sukrithajapam:"sukruthajapam of day 21",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 22",
+          advice : "advice of day 22",
+          prayer : "prayer of day 22",
+          sukrithajapam:"sukruthajapam of day 22",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 23",
+          advice : "advice of day 23",
+          prayer : "prayer of day 23",
+          sukrithajapam:"sukruthajapam of day 23",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 24",
+          advice : "advice of day 24",
+          prayer : "prayer of day 24",
+          sukrithajapam:"sukruthajapam of day 24",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 25",
+          advice : "advice of day 25",
+          prayer : "prayer of day 25",
+          sukrithajapam:"sukruthajapam of day 25",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 26",
+          advice : "advice of day 26",
+          prayer : "prayer of day 26",
+          sukrithajapam:"sukruthajapam of day 26",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 27",
+          advice : "advice of day 27",
+          prayer : "prayer of day 27",
+          sukrithajapam:"sukruthajapam of day 27",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 28",
+          advice : "advice of day 28",
+          prayer : "prayer of day 28",
+          sukrithajapam:"sukruthajapam of day 28",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 29",
+          advice : "advice of day 29",
+          prayer : "prayer of day 29",
+          sukrithajapam:"sukruthajapam of day 29",
+          salkriya:"Today's salkriya"
+      },
+      {
+          topic : "Topic of day 30",
+          advice : "advice of day 30",
+          prayer : "prayer of day 30",
+          sukrithajapam:"sukruthajapam of day 30",
+          salkriya:"Today's salkriya"
+      }
+  ]
+};
 
-`
-onload = function(){
-var header = document.getElementById("header").innerHTML = headerText;
+var luthiniya = "ലുത്തിനിയ";
+
+var contentDiv = document.getElementById("contentDiv");
+
+console.log(data);
+
+function display(x){
+    switch(x){
+        case 1:
+            { 
+                console.log("Prayer to help 1000 souls loaded");
+                contentDiv.innerHTML = `
+                        <p class="h5 text-center">ആയിരം ആത്മാക്കളുടെ  പ്രാർത്ഥന</p>
+
+                        <p class="p malayalam text-secondary blockquote"> ഒരുതവണ ചൊല്ലിയാൽ ആയിരം ആത്മാക്കളെ ശുദ്ധീകരണസ്ഥലത്തുനിന്നും മോചിപ്പിക്കുമെന്ന് വാഗ്ദാനം ചെയ്തുകൊണ്ട് ഈശോ വി. ജെത്രൂദിനെ പഠിപ്പിച്ച പ്രാർത്ഥന </p>
+                        <p class="malayalam fw-500 h5 text-wrap h5">
+                            നിത്യനായ ദൈവമേ ഇന്നേദിവസം അർപ്പിക്കപ്പെടുന്ന എല്ലാ ബലികളോടും ചേർത്ത് അങ്ങേ പ്രിയപുത്രനായ ഈശോമിശിഹായുടെ തിരുരക്തം ശുദ്ധീകരണസ്ഥലത്തുള്ള ആത്മാക്കൾക്കായും ലോകമെങ്ങുമുള്ള പാപികൾക്കും സഭയിലുള്ള പാപികൾക്കും എന്റെ കുടുംബത്തിലെയും എന്റെ ഭവനത്തിലെയും പാപികൾക്കായും ഞാൻ കാഴ്ചസമർപ്പിക്കുന്നു <br><br>
+                            1.സ്വർഗ്ഗ. 1.നന്മ 1.ത്രിത്വ 
+                        </p>
+                `;
+                break;
+            }
+        case 2:
+            {
+                console.log("Karunakontha loaded;");
+                contentDiv.innerHTML = `
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/nqRK-KeX7Hw?si=kVEFt0pYad5gsu0f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" loop allowfullscreen></iframe>
+                `;
+                break;
+            }
+    }
 }
+
+function novena(x){
+    
+    contentDiv.innerHTML = 
+    `
+        <div id="contentDiv" class="malayalam text-dark">
+        <p class="h3 malayalam text-center">നൊവേന</p>
+        <p class="h4">Day ${x}</p>
+        <p class="h5 letterSpacing">${data.novena[x-1]}</p>
+    `;
+
+    console.log("novena day " + x +" loaded;");
+  }
+
+
+  function vanakkmaasam(x){
+    contentDiv.innerHTML = 
+    `
+        <div class="vanakkamaasamDaysDiv">
+            <p class="h3 malayalam text-center">
+                വണക്കമാസം <br>
+                <span class="text-secondary">Nov ${x}</span>
+            </p>
+
+            <hr class="hr-default">
+
+            <p class="h5 malayalam">ഇന്നത്തെ ചിന്താവിഷയം</p>
+            <p class="malayalam " id="topic">
+                ${data.vanakkamaasam[x-1].topic}
+            </p>
+
+            <hr class="hr-default">
+
+            <p class="h5 malayalam">ഉപദേശം</p>
+            <p class="malayalam " id="upadesham">
+                ${data.vanakkamaasam[x-1].advice}
+            </p>
+
+            <hr class="hr-default">
+
+            <p class="h5 malayalam">പ്രാർത്ഥന</p>
+            <p class="malayalam " id="prayer">
+                ${data.vanakkamaasam[x-1].prayer}
+            </p>
+
+            <hr class="hr-default">
+
+            <p class="h5 malayalam">ലുത്തിനിയ</p>
+            <p class="" id="vanakkamasamConclution">
+                ${luthiniya}
+            </p>
+
+            <hr class="hr-default">
+
+            <p class="h5 malayalam">സുകൃതജപം</p>
+            <p class="malayalam " id="sukruthajapam">
+                ${data.vanakkamaasam[x-1].sukrithajapam}
+            </p>
+
+            <hr class="hr-default">
+
+
+            <p class="h5 malayalam">സൽക്രിയ </p>
+            <p class="malayalam " id="salkriya">
+                ${data.vanakkamaasam[x-1].salkriya}
+            </p>
+
+            <hr class="hr-default">
+        </div>
+
+    `
+    console.log("vanakkamaasam day " + x +" loaded;");
+  }
+  
+
